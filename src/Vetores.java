ed9a7fa23ec4b6f811270fb4e730bad1cb3d1fc8
Beatriz Cardoso Cunha <beacarcun@gmail.com>
2021-05-26 17:26:00 -0300

    Preparando para incluir um vetor.

--- src/Vetores.java
+++ src/Vetores.java
@@ -4,6 +4,28 @@ public class Vetores {
 
 	public static void main(String[] args) {
 		
+		Scanner leitor = new Scanner(System.in);
+		
+		int quantidadeAlunos = 0;
+		System.out.println("Digite a quantidade de alunos na turma:");
+		quantidadeAlunos = leitor.nextInt();
+		
+		double totalConsumo = 0; // ZERANDO
+		
+		for ( int i = 0; i < quantidadeAlunos; i++ ) {
+			int numeroAluno = i + 1; // TODO
+			System.out.println("Por favor, digite o consumo do " + numeroAluno +  "º aluno");
+			double valorConsumo = leitor.nextDouble();
+			System.out.println("O aluno " + numeroAluno + " consumiu R$ " + valorConsumo);
+			totalConsumo += valorConsumo;
+		}
+		
+		
+		System.out.println("Total: " + totalConsumo);
+		
+		leitor.close();
+		
+		/*
 		double aluno1, aluno2, aluno3, aluno4, aluno5, aluno6, aluno7, aluno8, aluno9, aluno10, total;
 		Scanner leitor = new Scanner(System.in);
 		
@@ -44,6 +66,7 @@ public class Vetores {
 		System.out.println("O total de consumo foi de R$" + total);
 		
 		leitor.close();
+		*/
 		
 
 	}
