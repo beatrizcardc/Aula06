8f5a7b2d9fb571185c9abb9ad0c5560087571437
Beatriz Cardoso Cunha <beacarcun@gmail.com>
2021-05-27 17:25:58 -0300

    Adicionando comentários e adequações da iteração dentro ou fora do FOR

--- src/Vetores.java
+++ src/Vetores.java
@@ -7,22 +7,33 @@ public class Vetores {
 		Scanner leitor = new Scanner(System.in);
 		
 		int quantidadeAlunos = 0;
+		double totalConsumo = 0; // ZERANDO. Essa varíavel que recebe a soma do total do consumo precisa ser criada fora do FOR porque senão zeraria a soma a cada loop e só retornaria como resultado o último valor digitado, e nunca uma soma.
 		System.out.println("Digite a quantidade de alunos na turma:");
 		quantidadeAlunos = leitor.nextInt();
 		
-		double totalConsumo = 0; // ZERANDO. Essa varíavel que recebe a soma do total do consumo precisa ser criada fora do FOR porque senão zeraria a soma a cada loop e só retornaria como resultado o último valor digitado, e nunca uma soma.
 		
+		double valores[] = new double[quantidadeAlunos]; // declaração de vetor array
+		int numeroAluno = 0;
 		for ( int i = 0; i < quantidadeAlunos; i++ ) {
-			int numeroAluno = i + 1; // TODO
+			//int numeroAluno = i + 1; manter aqui obriga a criação uma e outra vez da variável (diminui perfomance), ao ficar fora do FOR basta usar esta varíavel sem precisar criá-la novamente. Evitar fazer instancias dentro do FOR
+			numeroAluno = i + 1;
 			System.out.println("Por favor, digite o consumo do " + numeroAluno +  "º aluno");
-			double valorConsumo = leitor.nextDouble();
-			System.out.println("O aluno " + numeroAluno + " consumiu R$ " + valorConsumo);
+			//double valorConsumo = leitor.nextDouble();
+			//System.out.println("O aluno " + numeroAluno + " consumiu R$ " + valorConsumo);
 			//aqui iria a variável double totalConsumo = 0; mas só retornaria o último valor digitado e nunca a soma.
-			totalConsumo += valorConsumo;
+			//totalConsumo += valorConsumo; toda esa parte do código não é interessante ficar dentro do for
+			valores[i] = leitor.nextDouble();
+			totalConsumo = totalConsumo + valores[i];
+		}
+		System.out.println("***********");
+		System.out.println(valores.length);
+		
+		for (int i = 0; i < valores.length; i++) {
+			System.out.println("O consumo do aluno " + numeroAluno + " foi de R$" + valores[i]);
 		}
 		
 		
-		System.out.println("Total: " + totalConsumo);
+		System.out.println("*********Total R$: " + totalConsumo); //esse também fica fora do for para imprimir uma vez só
 		
 		leitor.close();
 		
